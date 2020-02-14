50aba58cd9b7f1b761cc995cc898bba2111c8a51
Lorien Cho <42721802+loriencho@users.noreply.github.com>
2020-02-14 10:54:07 -0500

    Commit

---
+++ Assets/EnemyMovement.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    private ProjectilePools projectilePools;
+        
+
+    void Start()
+    {
+        projectilePools = ProjectilePools.Instance;
+
+        InvokeRepeating("SpawnProjectile", 1.0f, 1.0f);
+
+        StartCoroutine("Waiter");
+        
+
+
+    }
+
+    IEnumerator Waiter() {
+
+        yield return new WaitForSeconds(3f);
+        
+        StartCoroutine("Move");
+
+
+
+    }
+
+    IEnumerator Move() {
+        StartCoroutine("Rotate");
+        while (gameObject.activeSelf)  {
+            transform.position = new Vector3(transform.position.x - .5f, transform.position.y, transform.position.z);
+            yield return new WaitForSeconds(.05f);
+
+        }
+    }
+
+    IEnumerator Rotate() {
+
+        while(gameObject.activeSelf) {
+
+            transform.Rotate(0, 0, 1);
+            yield return null;
+        }
+    }
+
+    void SpawnProjectile() {
+    
+        GameObject projectile;
+        
+
+        transform.position = new Vector3 (transform.position.x, transform.position.y, 10f);
+        
+        projectile = projectilePools.SpawnFromPool("EnemyProjectile", transform.position, Quaternion.identity);
+
+    }
+
+    void OnBecameInvisible()
+    {
+        enabled = false;
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/ReceiveAttack.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour
+{
+    public GameObject endCard;
+    public ParticleSystem explosion;
+
+    void Start()
+    {
+        explosion.Stop();
+        endCard.SetActive(false);
+        StartCoroutine("WaitForCollision");
+    }
+
+    IEnumerator WaitForCollision(){
+        LayerMask layerMask = LayerMask.GetMask("Projectiles");
+
+        while ( !(Physics2D.OverlapCircle(new Vector2(transform.position.x, transform.position.y), 1.4f, layerMask, 10f, 10f))) {
+            yield return null;
+
+        }
+        
+        explosion.transform.position = transform.position;
+        explosion.Play();
+
+        yield return new WaitForSeconds(1f);
+        explosion.Stop();
+
+        endCard.SetActive(true);
+        StartCoroutine("SlideIntoPlace");
+
+    }
+
+    IEnumerator SlideIntoPlace() {
+        float timeSinceStarted = 0f;
+
+        while (endCard.transform.position != Vector3.zero) {
+                
+            timeSinceStarted += Time.deltaTime;
+            endCard.transform.position = Vector3.Lerp(endCard.transform.position, Vector3.zero, timeSinceStarted);
+            yield return null;
+        }
+    }
+}
